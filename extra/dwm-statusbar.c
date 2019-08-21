93b9d13d05541b72fa2600b5b1c926ba96b5b182
Chunhui He <hchunhui@mail.ustc.edu.cn>
2019-08-21 08:05:59 +0000

    dwm-statusbar: low battery

--- extra/dwm-statusbar.c
+++ extra/dwm-statusbar.c
@@ -48,6 +48,8 @@ void readfile(char *s, char *fname)
 void setroot()
 {
 	char s1[200], s2[200], s3[200], s[200];
+	int cap;
+	static int count, count2;
 
 	readfile(s1, "/sys/class/power_supply/BAT0/status");
 	readfile(s2, "/sys/class/power_supply/BAT0/capacity");
@@ -56,6 +58,26 @@ void setroot()
 
 	XStoreName(dpy, root, s);
 	XFlush(dpy);
+
+	sscanf(s2, "%d", &cap);
+	readfile(s3, "/sys/class/power_supply/AC/online");
+
+	if (count > 0)
+		count--;
+	if (count2 > 0)
+		count2--;
+
+	if (cap < 15 && s3[0] == '0' && count == 0) {
+		sprintf(s, "notify-send -u critical 'Low Battery: %s%%'", s2);
+		system(s);
+		system("play /usr/share/sounds/freedesktop/stereo/dialog-warning.oga");
+		count = 300;
+	}
+
+	if (cap < 5 && s3[0] == '0' && count2 == 0) {
+		system("sudo systemctl hibernate");
+		count2 = 60;
+	}
 }
 
 int main(int argc, char *argv[])
