1b1a4cd01a2138e03b5da1ab0f25d0aac0611f2b
Chunhui He <hchunhui@mail.ustc.edu.cn>
2022-02-13 13:17:04 +0000

    update config

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=14:antialias=true";
+static char *font = "Monospace:pixelsize=12:antialias=true";
 static int borderpx = 2;
 
 /*
