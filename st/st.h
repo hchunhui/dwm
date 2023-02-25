d4d1146edda59ceb58d12601bfbdd5ab10fd1c4e
Chunhui He <hchunhui@mail.ustc.edu.cn>
2023-02-25 07:57:40 +0000

    Revert "st: solarized"
    This reverts commit c5f4f88405f1d8ad27907fb8a99c9c2393796c45.

--- st/st.h
+++ st/st.h
@@ -126,7 +126,6 @@ extern char *vtiden;
 extern char *worddelimiters;
 extern int allowaltscreen;
 extern char *termname;
-extern int usealtcolors;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
